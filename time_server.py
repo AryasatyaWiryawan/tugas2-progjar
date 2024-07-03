15ae1af733b90186fe2377e5e4655d513f808a9e
Aryasatya Wiryawan <aryasatyapark@gmail.com>
2024-07-04 01:45:24 +0700

    Update time_server.py

--- time_server.py
+++ time_server.py
@@ -1,33 +1,55 @@
+from socket import *
 import socket
 import threading
-from datetime import datetime
+import logging
+import time
+import sys
 
-def handle_client(client_socket):
-    with client_socket:
+class ProcessTheClient(threading.Thread):
+    def __init__(self, connection, address):
+        self.connection = connection
+        self.address = address
+        threading.Thread.__init__(self)
+
+    def run(self):
         while True:
-            request = client_socket.recv(1024)
-            if request:
-                request_str = request.decode('utf-8')
-                if request_str.startswith('TIME') and request_str.endswith('\r\n'):
-                    current_time = datetime.now().strftime('%H:%M:%S')
-                    response = f'JAM {current_time}\r\n'
-                    client_socket.send(response.encode('utf-8'))
-                elif request_str.startswith('QUIT') and request_str.endswith('\r\n'):
+            data = self.connection.recv(32)
+            if data:
+                decoded_data = data.decode('utf-8').strip()
+                if decoded_data == "QUIT":
                     break
+                elif decoded_data.startswith("TIME"):
+                    current_time = time.strftime("%H:%M:%S")
+                    response = f"JAM {current_time}\r\n"
+                    self.connection.sendall(response.encode('utf-8'))
+                else:
+                    self.connection.sendall(data)
             else:
                 break
+        self.connection.close()
 
-def start_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('0.0.0.0', 45000))
-    server.listen(5)
-    print('Server listening on port 45000')
+class Server(threading.Thread):
+    def __init__(self):
+        self.the_clients = []
+        self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        threading.Thread.__init__(self)
 
-    while True:
-        client_socket, addr = server.accept()
-        print(f'Accepted connection from {addr}')
-        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
-        client_handler.start()
+    def run(self):
+        self.my_socket.bind(('0.0.0.0', 45000))
+        self.my_socket.listen(1)
+        while True:
+            self.connection, self.client_address = self.my_socket.accept()
+            logging.warning(f"connection from {self.client_address}")
+            
+            clt = ProcessTheClient(self.connection, self.client_address)
+            clt.start()
+            self.the_clients.append(clt)
+    
+
+def main():
+    svr = Server()
+    svr.start()
 
 if __name__ == "__main__":
-    start_server()
+    logging.basicConfig(level=logging.WARNING, format='%(asctime)s %(message)s')
+    main()
